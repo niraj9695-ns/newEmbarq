989da0662d2019a5eaebe62eacfabc33fd546789
Omkar <omyewale333@gmail.com>
2026-05-05 13:12:42 +0530

    Gallery Section

--- src/pages/GallerySection.jsx
+++ src/pages/GallerySection.jsx
@@ -69,6 +69,8 @@ const GallerySection = () => {
   const [isHeroVisible, setIsHeroVisible] = useState(true);
 
   const [activeIndex, setActiveIndex] = useState(0);
+  const [autoDirection, setAutoDirection] = useState(1);
+  const [pauseAutoScroll, setPauseAutoScroll] = useState(false);
 
   const [backgroundImg] = useState(galleryHero);
 
@@ -219,6 +221,21 @@ const GallerySection = () => {
     setActiveIndex(0);
   };
 
+  useEffect(() => {
+    if (pauseAutoScroll) return undefined;
+
+    const autoScrollTimer = setInterval(() => {
+      if (autoDirection === 1) {
+        handleNext();
+      } else {
+        handlePrev();
+      }
+      setAutoDirection((prev) => prev * -1);
+    }, 3000);
+
+    return () => clearInterval(autoScrollTimer);
+  }, [autoDirection, pauseAutoScroll, destinations]);
+
   const GalleryItem = ({ img, rowSpan = 1, colSpan = 1 }) => (
     <Box
       sx={{
@@ -318,10 +335,16 @@ const GallerySection = () => {
                 width: "100%",
                 display: "flex",
                 justifyContent: { xs: "flex-start", md: "center" },
-                overflowX: { xs: "hidden", md: "hidden" },
+                overflowX: "auto",
                 px: { xs: 2, md: 0 },
                 scrollBehavior: "smooth",
+                "&::-webkit-scrollbar": { display: "none" },
+                scrollbarWidth: "none",
               }}
+              onMouseEnter={() => setPauseAutoScroll(true)}
+              onMouseLeave={() => setPauseAutoScroll(false)}
+              onTouchStart={() => setPauseAutoScroll(true)}
+              onTouchEnd={() => setPauseAutoScroll(false)}
             >
               <Box
                 sx={{
